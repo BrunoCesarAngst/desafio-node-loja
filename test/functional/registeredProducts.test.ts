aef7f0a9d990fe70f232fa100a20a42b6a0a4944
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2020-10-25 02:00:42 -0300

    Adicionando Products model com Mongoose e TS

--- test/functional/registeredProducts.test.ts
+++ test/functional/registeredProducts.test.ts
@@ -1,4 +1,17 @@
+import { Product } from "@src/models/products";
+
 describe('Product list functional testing ', () => {
+  beforeEach(async () => {
+    await Product.deleteMany({});
+    const defaultProduct: Product = {
+      name: 'Product A',
+      description: 'first test product',
+      price: 100.0,
+    };
+    const product = new Product(defaultProduct);
+    await product.save();
+  });
+
   it('should return a list of products', async () => {
     const { body, status } = await global.testRequest.get('/products');
     expect(status).toBe(200);
